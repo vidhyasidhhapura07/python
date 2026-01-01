26706d6ab829c4ec49dd8ce65f549dc12872b554
vidhyasidhhapura07 <vidhdhyasidhdhpura@gmail.com>
2026-01-01 12:21:44 +0530

    Add files via upload

---
+++ day5/forloop.py
@@ -0,0 +1,112 @@
+# # For Loops
+
+
+# subjet = ["English", "Computer", "Hindi", "Arrond Us", "Maths"]
+
+# for n in subjet :
+#     print(n)
+
+ # #  print 1 to 50 numbers with loop 
+
+
+# for N in range (1,51):
+#     print (N)
+    
+#  # print even no (1 to 50) 
+
+# def even():
+#     for N in range(1,51):
+#         if N % 2 == 0:
+#             print(N)
+# even()
+
+# # loop in to string
+
+
+# for n in "Arround Us" :
+#     print(n)
+
+# # The break Statement
+
+
+# for n in subjet :
+#     print(n)
+#     if n == "Hindi":
+#         break
+
+# # the continue statement
+
+
+# for n in subjet :
+#     if n == "Hindi":
+#         continue
+#     print(n)
+
+# # 2
+
+# for n in 'vidhya niket dodiya':
+#     if n == 'i' or n == 'y':
+#         continue
+#     print(n) 
+
+# # The range() Function
+
+
+# for n in range(10):
+#     print(n)
+
+# # 2
+
+
+# for n in range(1, 11): # first value is straing value and second value is  last minus(-) 1
+#     print(n)
+
+# # 3
+
+
+# for n in range (0, 51 , 5):  #first value is straing value and second value is  last minus(-) 1 third value is step like how many step ago dispaly no
+#     print(n)
+
+
+# # Else in For Loop
+
+# for n in range (0, 10):
+#     print(n)
+# else:
+#     print("Finally finished!")
+
+
+# # pass statment
+
+# for x in [0,1,2,3]:
+#     pass
+
+
+# # nested loops with string
+
+# color = ["red", "blue", "white", "yellow", "green"]
+# obj = ["bat", "ball"]
+
+# for x in color:
+#     for y in obj:
+#         print(x,y)
+
+# # nested loops with numbers
+
+
+# for n in range(1,11):
+#     for v in range(1 , 51):
+#         print(v , end =" ")
+#     print()
+
+
+# # Increasing Number with nested loop 
+
+for n in range(1,6):
+    for v in range(1, n + 1):
+        print(v , end =" ")
+    print()
+
+
+
+
---
+++ day5/whileloop.py
@@ -0,0 +1,58 @@
+# # print 1 to 10 no with while loop 
+
+# i = 1
+
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+# # 1 to 10 number's sum  with while loop 
+
+# def sum():
+#     n = 1
+#     sum = 0
+#     while n <= 10:
+#         sum += n
+#         n += 1
+#     print(sum)
+# sum()
+
+
+# # create multipication table 
+
+# n = int(input("enter any number for multiplicatin"))
+
+# def multipliction(n):
+#     for v in  range(1 , 11):
+#         print(f"{n} * {v} = {n*v}")
+# multipliction(n)
+
+
+# # The break Statement
+
+# n = 10  
+# while n < 15:
+#     print(n)
+#     if (n == 12):
+#         break
+#     n += 1
+
+
+# # The continue Statement
+
+# n = 9
+# while n < 15:
+#     n += 1
+#     if (n == 10):
+#         continue
+#     print(n)
+
+
+# # The else Statement
+
+# n = 1
+# while n < 10:
+#     print(n)
+#     n += 1
+# else:
+#     print("finished...!")
