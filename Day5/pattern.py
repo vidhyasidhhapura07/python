c571357dbc8a06a63723354346fe97a42ffe1067
vidhyasidhhapura07 <vidhdhyasidhdhpura@gmail.com>
2026-01-05 14:37:30 +0530

    Add files via upload

---
+++ Day5/pattern.py
@@ -0,0 +1,53 @@
+# for n in range(1 , 6):
+#     for v in range(1, n + 1):
+#         print(v, end= " ")
+#     print()
+
+# for n in range(6, 0, -1):
+#     for v in range(n):
+#         print(v, end=" ")
+#     print()
+
+# for n in range(5, 0, -1):
+#     for v in range(5, 5 - n, -1):
+#         print(v, end=" ")
+#     print()
+
+
+# row = 7
+
+# for n in range(1, row + 1):
+#     print(" " * (row - n ), end="")
+#     for v in range(1, n + 1):
+#         print(v, end=" ")
+#     print()
+
+
+# n = 4
+# for v in range(1, n + 1):
+#     print(" " * (2 *(n - v)), end=" ")
+#     for i in range(1,2 * v):
+#         print(i, end=" ")
+#     print()
+
+
+# rows = 4
+# for i in range(1, rows + 1):
+#     print(" " * (rows - i) + "*" * (2*i - 1))
+
+
+# num = 1
+# for i in range(1, 5):
+#     for j in range(i):
+#         print(num, end=" ")
+#         num += 1
+#     print()
+
+
+# n = 5
+# for v in range(1, n+1):
+#     print (" "* (2 *(n - v)), end=" ")
+#     for d in range(1,2 * v):
+#         print(d, end=" ")
+#     print()
+
